7cf5fb8409a76bdd4f075d74ec9c79478866e636
Mustafa Fatih Can <canmustafafatih@gmail.com>
2025-11-14 23:39:45 +0300

    falling weights and animation

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+const seesaw = document.getElementById('seesaw-input');
+
+seesaw.addEventListener('click', function(event) {
+    const rect = seesaw.getBoundingClientRect();
+    const clickX = event.clientX - rect.left;
+
+    const obj = document.createElement('div');
+    obj.classList.add('weight');
+    obj.style.left = `${clickX - 15}px`;
+    obj.style.bottom = '500px';
+
+    setTimeout(() => {
+        obj.style.bottom = '0px';
+    }, 10);
+
+    seesaw.appendChild(obj);
+});
